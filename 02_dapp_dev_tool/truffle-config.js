d0a91c9f80ae710f6a4200c1e467b97651b62958
Wayne Chiu <chaoweichiu@gmail.com>
2019-11-21 08:56:15 +0800

    add hello world truffle example

---
+++ 02_dapp_dev_tool/truffle-config.js
@@ -0,0 +1,29 @@
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"
+var provider = new HDWalletProvider(mnemonic, "http://localhost:8545");
+
+module.exports = {
+  networks: {
+    development: {
+      provider:provider,
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+}

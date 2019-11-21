573a9901deb909328e4d186a02604799c846455c
Wayne Chiu <chaoweichiu@gmail.com>
2019-11-21 08:17:37 +0800

    fix wording

--- guides/02_send_eth.js
+++ guides/02_send_eth.js
@@ -9,7 +9,7 @@ var web3 = new Web3(provider);
 web3.setProvider(provider);
 web3.eth.sendTransaction(
   {
-    from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", // private key -> 0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3,
+    from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57", // private key -> 0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3
     to: "0xf17f52151EbEF6C7334FAD080c5704D77216b732",
     value: web3.utils.toWei("1", "ether")
   },
